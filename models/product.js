51dc364780864077ad704b49150aa35d12ae085f
oshinpojta <sujanian785@gmail.com>
2022-08-15 02:55:50 +0530

    one to many realtionship

--- models/product.js
+++ models/product.js
@@ -4,68 +4,24 @@ const sequelize = require('../util/database');
 
 const Product = sequelize.define('product', {
   id: {
-    type : Sequelize.INTEGER,
-    autoIncrement : true,
-    allowNull : false,
-    primaryKey : true
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
   },
-  title : {
-    type : Sequelize.STRING,
-    allowNull : false
-  },
-  price : {
+  title: Sequelize.STRING,
+  price: {
     type: Sequelize.DOUBLE,
-    allowNull : false
+    allowNull: false
   },
-  imageUrl : {
-    type : Sequelize.STRING,
-    allowNull : false
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
   },
-  description : {
-    type : Sequelize.STRING,
-    allowNull : false
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
 });
 
 module.exports = Product;
-
-
-
-
-
-
-// const fs = require('fs');
-// const path = require('path');
-// const db = require('../util/database');
-
-// const Cart = require('./cart');
-
-// module.exports = class Product {
-//   constructor(id, title, imageUrl, description, price) {
-//     this.id = id;
-//     this.title = title;
-//     this.imageUrl = imageUrl;
-//     this.description = description;
-//     this.price = price;
-//   }
-
-//   save(product) {
-//     return db.execute('INSERT INTO PRODUCTS (title, price, description, imageUrl) VALUES (?, ?, ?, ?)',[product.title, product.price, product.description, product.imageUrl]);
-//   }
-
-//   update(product) {
-//     return db.execute('UPDATE PRODUCTS SET title = ?, price = ?, description = ?, imageUrl = ? where id = ?', [product.title, product.price, product.description, product.imageUrl, product.id]);
-//   }
-
-//   static deleteById(id) {
-//     return db.execute('DELETE FROM PRODUCTS WHERE id = ?',[id]);
-//   }
-
-//   static fetchAll() {
-//     return db.execute('SELECT * FROM PRODUCTS')
-//   }
-
-//   static findById(id) {
-//     return db.execute('SELECT * FROM PRODUCTS WHERE id = ?',[id]);
-//   }
-// };
--- util/database.js
+++ util/database.js
@@ -1,22 +1,8 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'darkknight2785', {
-    dialect : 'mysql',
-    host : 'localhost'
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
 module.exports = sequelize;
-
-
-
-
-// const mysql = require("mysql2");
-
-// const pool = mysql.createPool({
-//     host : 'localhost',
-//     user : 'root',
-//     database : 'node-complete',
-//     password : 'darkknight2785'
-// });
-
-// module.exports = pool.promise();
